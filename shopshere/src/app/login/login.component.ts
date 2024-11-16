ac6250f1dedc50d2f1fb897aad3703d0176dd379
serhatkocak <sirhat5885@gmail.com>
2024-11-16 21:41:43 +0300

    login finish

--- shopshere/src/app/login/login.component.ts
+++ shopshere/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      remember:[false]
+    });
+
+  }
+
+  getControlName(controlName:string){
+    const control = this.loginForm.controls[controlName]
+    console.log(control.invalid && control.touched)
+    return control.invalid && control.touched;
+  }
+
+  
+  onSubmit(){
+  }
 
 }
